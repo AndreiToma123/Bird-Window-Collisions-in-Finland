515274986e707e4e9db161306ce98d6672e0907e
tia <yatianzheng3@gmail.com>
2024-03-10 22:05:33 +0200

    Beautify report page

--- userfront1/src/componment/report.js
+++ userfront1/src/componment/report.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { MapContainer, TileLayer, useMapEvents, useMap, Marker} from 'react-leaflet';
 import L from 'leaflet';
-import customIconUrl from '../Picture/Map_Location_Symbol.png'; // 更换为您的图标路径
+import customIconUrl from '../Picture/Map_Location_Symbol.png';
 import 'leaflet/dist/leaflet.css';
 import './report.css'; 
 
@@ -19,15 +19,16 @@ function UploadForm() {
 
   const customIcon = L.icon({
     iconUrl: customIconUrl,
-    iconSize: [38, 95], // 根据需要调整大小
-    iconAnchor: [22, 94], // 根据需要调整锚点位置
-    popupAnchor: [-3, -76] // 根据需要调整弹出窗口的锚点位置
+    iconSize: [38, 95], 
+    iconAnchor: [22, 94], 
+    popupAnchor: [-3, -76] 
   });
 
   function MapUpdater({ center }) {
     const map = useMap();
     useEffect(() => {
       map.setView(center);
+      map.invalidateSize();
     }, [center, map]);
   
     return null;
@@ -36,7 +37,7 @@ function UploadForm() {
   const LocationPicker = () => {
     useMapEvents({
       click(e) {
-        setSelectedLocation(e.latlng); // 当用户点击地图时更新 selectedLocation
+        setSelectedLocation(e.latlng);
       },
     });
     return null;
@@ -67,7 +68,7 @@ function UploadForm() {
           setLocation((prevState) => ({
             ...prevState,
             loaded: true,
-            coordinates: { lat: 60.1699, lng: 24.9384 }, // 设置默认坐标为赫尔辛基
+            coordinates: { lat: 60.1699, lng: 24.9384 }, 
             error: 'Location permission denied or error occurred'
           }));
         }
@@ -76,13 +77,18 @@ function UploadForm() {
   }, []);
   
 
-  const handleImageChange = (event, setImage) => {
-    setImage(URL.createObjectURL(event.target.files[0]));
+  const handleImageChange = (imageType, event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const imageSetter = imageType === 'head' ? setHeadImage
+                          : imageType === 'body' ? setBodyImage
+                          : setTailImage;
+      imageSetter(URL.createObjectURL(file));
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Submit logic goes here...
   };
 
   const handleDescriptionChange = (event) => {
@@ -98,51 +104,39 @@ function UploadForm() {
 
   return (
     <div className="form-container">
+      <div className="left-container">
       <form onSubmit={handleSubmit}>
         <div className="image-upload-container">
-          {/* Image upload for Head */}
-          <div className="image-upload">
-            {headImage && <img src={headImage} alt="Head Preview" className="image-preview" />}
-            <label>
-              Head
+          {['head', 'body', 'tail'].map((part) => (
+            <div className="image-upload" key={part}>
               <input
+                id={`image-upload-${part}`}
                 type="file"
                 accept="image/*"
-                onChange={(e) => handleImageChange(e, setHeadImage)}
+                onChange={(e) => handleImageChange(part, e)}
+                className="image-upload-input"
               />
-            </label>
-          </div>
-  
-          {/* Image upload for Body */}
-          <div className="image-upload">
-            {bodyImage && <img src={bodyImage} alt="Body Preview" className="image-preview" />}
-            <label>
-              Body
-              <input
-                type="file"
-                accept="image/*"
-                onChange={(e) => handleImageChange(e, setBodyImage)}
-              />
-            </label>
-          </div>
-  
-          {/* Image upload for Tail */}
-          <div className="image-upload">
-            {tailImage && <img src={tailImage} alt="Tail Preview" className="image-preview" />}
-            <label>
-              Tail
-              <input
-                type="file"
-                accept="image/*"
-                onChange={(e) => handleImageChange(e, setTailImage)}
-              />
-            </label>
-          </div>
+              <label htmlFor={`image-upload-${part}`} className="image-upload-label">
+                  <div className="image-upload-plus">+</div>
+                </label>
+                {part === 'head' && headImage && (
+                  <img src={headImage} alt="Head Preview" className="image-preview" />
+                )}
+                {part === 'body' && bodyImage && (
+                  <img src={bodyImage} alt="Body Preview" className="image-preview" />
+                )}
+                {part === 'tail' && tailImage && (
+                  <img src={tailImage} alt="Tail Preview" className="image-preview" />
+                )}
+                <label htmlFor={`image-upload-${part}`} className="image-upload-text">
+                  {part.charAt(0).toUpperCase() + part.slice(1)}
+                </label>
+            </div>
+          ))}
         </div>
   
-        {/* Description Text Area */}
         <div className="description-container">
-          <h3>Description</h3>
+          <h4>Description</h4>
           <textarea
             placeholder="Enter your description here"
             value={description}
@@ -151,8 +145,26 @@ function UploadForm() {
           />
         </div>
 
-        {/* 地图容器 */}
-        <div className="map-container" style={{ height: '400px', width: '100%' }}>
+        <div>
+          {selectedLocation && (
+          <p>Selected Coordinates: Latitude: {selectedLocation.lat}, Longitude: {selectedLocation.lng}</p>
+          )}
+          <button onClick={handleSaveLocation} className="save-location-button">Save Location</button>
+        </div>
+
+        <div>
+          <h4>Location data:</h4>
+          {location.loaded ? (
+            location.error ? <p>{location.error}</p> : <p>Latitude: {location.coordinates.lat}, Longitude: {location.coordinates.lng}</p>
+          ) : (
+            <p>Loading location...</p>
+          )}
+        </div>
+
+        </form>
+      </div>
+
+      <div className="map-container">
           <MapContainer center={[location.coordinates.lat, location.coordinates.lng]} zoom={13} style={{ height: '100%', width: '100%' }}>
             <TileLayer
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -166,29 +178,9 @@ function UploadForm() {
           </MapContainer>
         </div>
 
-        {/* 显示选中坐标 */}
-        <div>
-          {selectedLocation && (
-          <p>Selected Coordinates: Latitude: {selectedLocation.lat}, Longitude: {selectedLocation.lng}</p>
-          )}
-          <button onClick={handleSaveLocation}>Save Location</button>
-        </div>
-
-        {/* 显示当前位置数据 */}
-        <div>
-          <h4>Location data:</h4>
-          {location.loaded ? (
-            location.error ? <p>{location.error}</p> : <p>Latitude: {location.coordinates.lat}, Longitude: {location.coordinates.lng}</p>
-          ) : (
-            <p>Loading location...</p>
-          )}
-        </div>
-
-        {/* 提交按钮 */}
         <div className="submit-button-container">
           <button type="submit">Submit</button>
         </div>
-      </form>
     </div>
   );
   
