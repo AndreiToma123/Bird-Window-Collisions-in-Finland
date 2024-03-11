bbbeacb8bb06b6e9621b69d20538e8dd507da699
JP793 <140292653+JP793@users.noreply.github.com>
2024-03-11 13:59:53 +0200

    Update About.js
    This page has been completed.

--- userfront1/src/About.js
+++ userfront1/src/About.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './About.css';
+
+
 
 function About() {
-  return <div>About Us Page Content</div>;
+  return (
+    <div className="about-container">
+      <h2>About us</h2>
+      <p>Welcome to WingSave, a pioneering website and mobile application dedicated to addressing the overlooked issue of bird-window collisions in Finland. Our mission is simple yet impactful: to gather essential data on these incidents to protect our feathered friends and preserve biodiversity.</p>
+      <h2>Our Features:</h2>
+      <ul>
+        <li>Multilingual Interface: Navigate easily in English, Finnish, or Swedish.</li>
+        <li>Simple Observation Submission: Upload photos and details of collision events with just a few taps.</li>
+        <li>Educational Insight: Learn about the species involved and how we can prevent future collisions.</li>
+      </ul>
+      <h2>Join us in making a difference. Every observation counts.</h2>
+    </div>
+  );
 }
 
 export default About;
