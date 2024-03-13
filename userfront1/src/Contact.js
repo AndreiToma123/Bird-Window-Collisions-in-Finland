28c0c6e56b71fb98b4e8f55b820ce885bd92e99a
Anh Vainionpaa <anh.vainionpaa@gmail.com>
2024-03-13 17:05:09 +0200

    Adding info for Contact page

--- userfront1/src/Contact.js
+++ userfront1/src/Contact.js
@@ -1,7 +1,25 @@
+// Contact.js
 import React from 'react';
+import styles from './Contact.css';
 
-function Contact() {
-  return <div>Contact Page Content</div>;
-}
+const Contact = () => {
+  return (
+    <div className="contact-container">
+      <h1 className="header">Contact information</h1>
+      <p className="intro">
+        In case of any issues regarding the application, data, questions, please contact us through:
+      </p>
+      <div className="contactInfo">
+        <p className="phone">
+          <span className={styles.icon}>📞</span> 045962xxxx
+        </p>
+        <p className="email">
+          <span className={styles.icon}>📧</span> example@xxx.com
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
+
